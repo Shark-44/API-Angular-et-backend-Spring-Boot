eb82df02341c6cf579c57249b48cd61900a20aa6
Shark-44 <joanny.bernardeau@gmail.com>
2024-08-22 19:21:30 +0200

    quelques retouches css avant de preparer a docker

--- src/app/language-list/language-list.component.ts
+++ src/app/language-list/language-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+import { Langage } from '../models/student.model';
 
 @Component({
   selector: 'app-language-list',
   standalone: true,
-  imports: [],
+  imports: [ NgFor],
   templateUrl: './language-list.component.html',
   styleUrl: './language-list.component.css'
 })
-export class LanguageListComponent {
+export class LanguageListComponent implements OnInit {
+Langages: Langage[] = []
 
+constructor(
+  private http: HttpClient
+) {}
+
+ngOnInit() {
+  this.fetchLangages();
+}
+
+fetchLangages() {
+  this.http.get<Langage[]>('http://localhost:8080/api/langages').subscribe({
+    next: (data) => {
+      this.Langages = data;
+    },
+    error: (error) => {
+      console.error('Error fetching all langages:', error);
+    }
+  });
+}
 }
