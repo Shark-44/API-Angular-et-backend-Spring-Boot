7605611792bcfee8691ec1ad0c1798684103bb8c
Shark-44 <joanny.bernardeau@gmail.com>
2024-09-03 08:25:59 +0200

    Remove environment.prod.ts from tracking

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://monjavaangular.duckdns.org/api',
-    apiUrl2: 'https://monjavaangular.duckdns.org',
-    imageUrl: 'https://monjavaangular.duckdns.org/uploads/images'
-  };
